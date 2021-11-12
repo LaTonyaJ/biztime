f554f2f274b478ae68b8d5f6d6beccbed0ffcf6f
LaTonya Johnson <latonya.johnson0921@gmail.com>
2021-11-11 19:46:00 -0600

    industy route

--- routes/industries.js
+++ routes/industries.js
@@ -3,27 +3,27 @@ const express = require('express');
 const router = new express.Router();
 const ExpressError = require('../expressError');
 
+//Get All Industries with company codes
 router.get('/', async(req, res, next) => {
     try{
-        const results = await db.query(`SELECT code, industry FROM industries`);
-
-        const companies = await db.query(`SELECT c.code, c.name, i.industry FROM companies AS c LEFT JOIN comp_industry AS ci ON c.code = ci.comp_code LEFT JOIN industries AS i ON i.code = ci.ind_code`);
-
-        let industry = results.rows;
-
-        let comps = companies.rows.map(c => c.name);
-    
+        
+        const results = await db.query(`SELECT c.code AS company_code, i.code AS industry_code, i.industry AS industry FROM companies AS c RIGHT JOIN comp_industry AS ci ON c.code = ci.comp_code RIGHT JOIN industries AS i ON i.code = ci.ind_code`);
 
         if(results.rows.length === 0){
             throw new ExpressError('No Industries Found', 404);
         }
-        return res.json({'industries': industry});
+
+        let industry = results.rows;
+
+        return res.json({'industries': industry });
         
     }catch(e){
         return next(e);
     }
 })
 
+
+//Add a new industry
 router.post('/', async(req, res,next) => {
     try{
         if(!req.body){
@@ -34,6 +34,19 @@ router.post('/', async(req, res,next) => {
         const result = await db.query(`INSERT INTO industries (code, industry) VALUES ($1,$2) RETURNING code, industry`,[code, industry]);
 
         return res.status(201).json({"industry": result.rows[0]})
+    }catch(e){
+        return next(e);
+    }
+})
+
+//Associate industry and company
+router.post('/associate', async(req, res, next) => {
+    try{
+        let {comp_code, ind_code} = req.body;
+
+        const result = await db.query(`INSERT INTO comp_industry (comp_code, ind_code) VALUES ($1, $2) RETURNING comp_code, ind_code`, [comp_code, ind_code]);
+
+        return res.josn(result.rows[0]);
     }catch(e){
         return next(e);
     }
